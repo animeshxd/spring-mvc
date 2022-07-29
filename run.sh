ed13c3b22f8fe76a60e3fb7b4fcc84583305e6a2
Animesh Murmu <am2646374@gmail.com>
2022-07-29 12:56:02 +0530

    updated runner

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
+TPID="$(ps -ef | awk '/[t]omcat/{print $2}')"
 
-if  [[ -z "$TOMCAT" ]]; then
+if  [ -z "$TOMCAT" ]; then
     echo "TOMCAT=/path/to/tomcat/ required " >&2
     exit -1
 fi
@@ -7,12 +8,14 @@ fi
 mvn clean package -DskipTests -Dmaven.test.skip -Dmaven.artifact.threads=30
 
 WAR="$(ls target/*.war | xargs -n 1 basename)"
-if [ $? != 0 ]; then 
-    echo ".war file not found in ./target directory"
+if [ $? -ne 0 ]; then 
+    echo ".war file not found in ./target directory" >&2
     exit -1
 fi
 rm $TOMCAT/webapps/${WAR%%.*}* -rf
 cp target/$WAR $TOMCAT/webapps/
-
-$TOMCAT/bin/catalina.sh stop &> /dev/null
-$TOMCAT/bin/catalina.sh run
+if ! [ $TPID -gt 0 ]; then
+    $TOMCAT/bin/catalina.sh run
+    exit 0
+fi
+echo "Deployed at http://localhost:8080/${WAR%%.*}"
