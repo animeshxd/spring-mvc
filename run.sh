670007d88af1da4d51ddfcdeac359443589b1a5e
Animesh Murmu <am2646374@gmail.com>
2022-07-29 10:23:09 +0530

    tomcat runner

--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
-mvn clean package tomcat7:run -DskipTests -Dmaven.test.skip -Dmaven.artifact.threads=30 
+
+if  [[ -z "$TOMCAT" ]]; then
+    echo "TOMCAT=/path/to/tomcat/ required " >&2
+    exit -1
+fi
+
+mvn clean package -DskipTests -Dmaven.test.skip -Dmaven.artifact.threads=30
+
+WAR="$(ls target/*.war | xargs -n 1 basename)"
+if [ $? != 0 ]; then 
+    echo ".war file not found in ./target directory"
+    exit -1
+fi
+rm $TOMCAT/webapps/${WAR%%.*}* -rf
+cp target/$WAR $TOMCAT/webapps/
+
+$TOMCAT/bin/catalina.sh stop &> /dev/null
+$TOMCAT/bin/catalina.sh run
