b600b004173fb8edd4e425d655e5c399b84d0199
Animesh Murmu <am26464374@gmail.com>
2022-08-15 10:23:28 +0530

    redirect using RedirectView

--- src/main/java/com/example/Index.java
+++ src/main/java/com/example/Index.java
@@ -2,11 +2,14 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class Index {
 	@GetMapping("/")
-	public String index() {
-		return "redirect:/blog/";
+	public RedirectView index() {
+		var redirectView = new RedirectView();
+		redirectView.setUrl("blog/");
+		return redirectView;
 	}
 }
