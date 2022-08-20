0561c6019c76353e3c392c3afba11561dc30fcf3
Animesh Murmu <am26464374@gmail.com>
2022-08-20 13:32:06 +0530

    New: x.java - print objects

---
+++ src/main/java/com/example/x.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class x {
+    /** print with [System.out.println] */ 
+    public static void print(Object... obj){
+        for (Object object : obj) {
+            System.out.println(object);
+        }
+    }
+}
